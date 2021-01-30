400652c1a544e1cdb7d5babfa10ca9df5a85d824
JiwonDev <sylvester7412@gmail.com>
2021-01-30 23:21:19 +0900

    기초수학 -단어길이재기

---
+++ 2. 기초수학/17_2743_단어길이재기.cpp
@@ -0,0 +1,16 @@
+//
+// Created by sylve on 2021-01-30.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main () {
+    int lowers, uppers, numbers, spaces;
+    string str;
+    cin >> str;
+    cout << str.length();
+}
