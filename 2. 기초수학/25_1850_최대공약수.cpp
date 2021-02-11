6adc35caa5848c1e1150b8c9cbee6aa2399d85ec
JiwonDev <sylvester7412@gmail.com>
2021-02-11 22:32:22 +0900

    기초수학 -최대공약수

---
+++ 2. 기초수학/25_1850_최대공약수.cpp
@@ -0,0 +1,27 @@
+//
+// Created by sylve on 2021-02-11.
+//
+
+#include<iostream>
+using namespace std;
+
+int gcd(long long a, long long b) {
+    if (a%b == 0) {
+        return b;
+    }
+
+    return gcd(b, a%b);
+}
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+
+    long long result = gcd(a, b);
+    for (int i = 0; i < result; i++) {
+        cout << 1;
+    }
+    cout << endl;
+
+    return 0;
+}
